787fee467ce8c94dc103da6e91237c6baff981d2
BjornKlarstrom <71707050+BjornKlarstrom@users.noreply.github.com>
2021-04-28 01:39:38 +0200

    Update Program.cs

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace TinyBrowser
 {
-    class Program
+    internal static class TinyBrowser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Tiny Browser!");
+            
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("GET / HTTP/0.9");
+            stringBuilder.AppendLine("Host: www.acme.com");
+            stringBuilder.AppendLine("Connection: close");
+            stringBuilder.AppendLine();
+            
+            Console.WriteLine(stringBuilder.ToString());
+            
+            
+            var tcpClient = new TcpClient("acme.com",80);
+
+            var stream = tcpClient.GetStream();
+
+            var message = Encoding.ASCII.GetBytes(stringBuilder.ToString());
+            
+            stream.Write(message,0,message.Length);
+            
+
+            // Receive
+            var memory = new MemoryStream();
+            stream.CopyTo(memory);
+            memory.Position = 0;
+
+            var data = memory.ToArray();
+            Console.WriteLine(Encoding.ASCII.GetString(data,0,data.Length));
+            
         }
     }
 }
