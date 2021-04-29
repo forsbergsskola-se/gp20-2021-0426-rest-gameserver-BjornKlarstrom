e73271cd2fcebfff368e6a1378f9b39a0543960f
BjornKlarstrom <71707050+BjornKlarstrom@users.noreply.github.com>
2021-04-29 23:53:11 +0200

    adds restructure to Program.cs

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -14,7 +14,7 @@ namespace TinyBrowser
         static NetworkStream stream;
         static StreamReader streamReader;
         static StreamWriter streamWriter;
-
+        
         const string hostUrl = "www.acme.com";
         const int tcpPort = 80;
         
@@ -31,69 +31,15 @@ namespace TinyBrowser
                 
             // Get the response
             var response = GetResponseFromWebSite();
-            Console.WriteLine(response);
-
             Console.WriteLine(SearchForTitle(response));
 
+            // Get and Print Links
+            var links = FindAllLinksWithTitles(response).ToArray();
+            for (var i = 0; i <= links.Length; i++){
+                Console.WriteLine($"{i}: {links[i].displayText} ({links[i].urlLink})");
+            }
+
             CloseApplication();
-            
-            
-            
-            
-            
-            // Searches
-            static string SearchForTitle(string str){
-                const string openElement = "<title>";
-                const string closeElement = "</title>";
-                
-                var startIndex = str.IndexOf(openElement, StringComparison.Ordinal);
-                startIndex += openElement.Length;
-                
-                var endIndex = str.IndexOf(closeElement, StringComparison.Ordinal);
-
-                return str.Substring(startIndex, (endIndex - startIndex));
-            }
-
-
-            var links = FindAllLinksWithTitles(response);
-            foreach (var link in links){
-                Console.WriteLine(link.title);
-            }
-            
-            // Example...
-            // <b>The <a href="build_a_pc/boardfinder/">ACME Motherboard Finder</a>.</b>
-
-            static IEnumerable<LinkAndTitle> FindAllLinksWithTitles(string response) { 
-                
-                const string linkStartTag= "<a href=\"";
-                const char quote = '"';
-                const char titleStartTag = '>';
-                const string titleEndTag = "</a>";
-                
-                
-                var listOfLinks = new List<LinkAndTitle>();
-                
-                var splitArray = response.Split(linkStartTag);
-                splitArray = splitArray.Skip(1).ToArray();
-
-                foreach (var split in splitArray){
-                    var link = split.TakeWhile(symbol => symbol != quote).ToArray();
-                    var afterLinkPart = split[link.Length..];
-                    var titleStartIndex = afterLinkPart.IndexOf(titleStartTag) + 1;
-                    var titleEndIndex = afterLinkPart.IndexOf(titleEndTag, StringComparison.Ordinal);
-                    var title = afterLinkPart.Substring(titleStartIndex,(titleEndIndex - titleStartIndex))
-                        .Replace("<b>", string.Empty).Replace("</b>", string.Empty);
-
-                    if (title.StartsWith("<img")){
-                        title = "Img";
-                    }
-                    listOfLinks.Add(new LinkAndTitle{
-                        link = new string(link),
-                        title = new string(title)
-                    });
-                }
-                return listOfLinks;
-            }
         }
 
         static void SetupTcpClient(){
@@ -111,7 +57,9 @@ namespace TinyBrowser
             var request = "GET / HTTP/1.1\r\n";
             request += "Host:"+ hostUrl +"\r\n";
             request += "\r\n";
-            stream.Write(Encoding.ASCII.GetBytes(request));
+
+            streamWriter.AutoFlush = true;
+            streamWriter.Write(request);
         }
         
         static string GetResponseFromWebSite() {
@@ -123,6 +71,53 @@ namespace TinyBrowser
             return string.Empty;
         }
         
+        // Searches
+        static string SearchForTitle(string str){
+            const string openElement = "<title>";
+            const string closeElement = "</title>";
+                
+            var startIndex = str.IndexOf(openElement, StringComparison.Ordinal);
+            startIndex += openElement.Length;
+                
+            var endIndex = str.IndexOf(closeElement, StringComparison.Ordinal);
+
+            return str.Substring(startIndex, (endIndex - startIndex));
+        }
+        
+        // HTML Example...
+        // <b>The <a href="build_a_pc/boardfinder/">ACME Motherboard Finder</a>.</b>
+        static IEnumerable<LinkAndTitle> FindAllLinksWithTitles(string response) { 
+                
+            const string linkTag = "<a href=\"";
+            const char quote = '"';
+            const char titleStartTag = '>';
+            const string titleEndTag = "</a>";
+                
+                
+            var listOfLinks = new List<LinkAndTitle>();
+                
+            var splitArray = response.Split(linkTag);
+            splitArray = splitArray.Skip(1).ToArray();
+
+            foreach (var split in splitArray){
+                var url = split.TakeWhile(symbol => symbol != quote).ToArray();
+                var afterUrlPart = split[url.Length..];
+                var displayTextStart = afterUrlPart.IndexOf(titleStartTag) + 1;
+                var displayTextEnd = afterUrlPart.IndexOf(titleEndTag, StringComparison.Ordinal);
+                var displayText = afterUrlPart.Substring(displayTextStart,(displayTextEnd - displayTextStart))
+                    .Replace("<b>", string.Empty).Replace("</b>", string.Empty);
+
+                if (displayText.StartsWith("<img")){
+                    displayText = "Img";
+                }
+                listOfLinks.Add(new LinkAndTitle{
+                    urlLink = new string(url),
+                    displayText = new string(displayText)
+                });
+            }
+            return listOfLinks;
+        }
+        
         static void CloseApplication(){
             Console.WriteLine("Closing Application...");
             tcpClient.Close();
@@ -131,7 +126,7 @@ namespace TinyBrowser
     }
 
     public class LinkAndTitle{
-        public string link;
-        public string title;
+        public string urlLink;
+        public string displayText;
     }
 }
