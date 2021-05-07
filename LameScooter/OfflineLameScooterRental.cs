857acbf5977b358ab362036d4304b0b065966744
BjornKlarstrom <71707050+BjornKlarstrom@users.noreply.github.com>
2021-05-07 12:30:18 +0200

    adds base classes for scooterstations

---
+++ LameScooter/OfflineLameScooterRental.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace LameScooter{
+    public class OfflineLameScooterRental : ILameScooterRental{
+
+        async Task<Stations> ScooterDataToList(){
+            return JsonConvert.DeserializeObject<Stations>(await File.ReadAllTextAsync("scooter.json"));
+        }
+        public Task<int> GetScooterCountInStation(string stationName){
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ LameScooter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LameScooter{
+
+    class Program{
+        static async Task Main(string[] args){
+
+            ILameScooterRental scooterRental = new OfflineLameScooterRental();
+
+            var scooterCount = scooterRental.GetScooterCountInStation(null); // Put command line arguments here
+            Console.WriteLine($"Number of scooter available at this station is: {scooterCount}");
+        }
+    }
+    
+    public interface ILameScooterRental{
+        Task<int> GetScooterCountInStation(string stationName);    
+    }
+    
+    public class Stations{
+        public List<ScooterStation> StationList{ get; set; }
+    }
+
+    public class ScooterStation{
+        public string Id               { get; set; }
+        public string Name             { get; set; }
+        public float  X                { get; set; }
+        public float  Y                { get; set; }
+        public int    BikesAvailable   { get; set; }
+        public int    SpacesAvailable  { get; set; }
+        public int    Capacity         { get; set; }
+        public bool   AllowDropOff     { get; set; }
+        public bool   AllowOverloading { get; set; }
+        public bool   IsFloatingBike   { get; set; }
+        public bool   IsCarStation     { get; set; }
+        public string State            { get; set; }
+    }
+}
