4264486d2ad42397663bda835259a8d85e999fc4
BjornKlarstrom <71707050+BjornKlarstrom@users.noreply.github.com>
2021-05-14 12:04:44 +0200

    adds PlayersController class

---
+++ MMORPG/PlayersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MMORPG{
+    public class PlayersController{
+
+        IRepository repository;
+        public PlayersController(IRepository repository){
+            this.repository = repository;
+        }
+
+        [HttpGet("Get")]
+        public Task<Player> Get(Guid id){
+            return repository.Get(id);
+        }
+
+        [HttpGet("GetAll")]
+        public Task<Player[]> GetAll(){
+            return repository.GetAll();
+        }
+
+        [HttpPost("Create")]
+        public Task<Player> Create(NewPlayer player){
+            
+            var newPlayer = new Player{
+                Name = player.Name,
+                CreationTime = DateTime.Now,
+                Id = Guid.NewGuid()
+            };
+            return repository.Create(newPlayer);
+        }
+
+        [HttpPut("Modify")]
+        public Task<Player> Modify(Guid id, ModifiedPlayer player){
+            return repository.Modify(id, player);
+        }
+
+        [HttpDelete("Delete")]
+        public Task<Player> Delete(Guid id){
+            return repository.Delete(id);
+        }
+    }
+}
--- MMORPG/Program.cs
+++ MMORPG/Program.cs
@@ -21,4 +21,6 @@ namespace MMORPG
                     webBuilder.UseStartup<Startup>();
                 });
     }
+    
+    
 }
